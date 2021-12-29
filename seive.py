7ee9fc9e188f98072cec28ed5ea095500fef8b8f
Stephen Maher <srqcoder@gmail.com>
2021-12-29 17:03:57 -0500

    Added Sieve.py

---
+++ seive.py
@@ -0,0 +1,11 @@
+'''
+	Sieve of Eratosthenes
+	an prime number identifier algorithm
+'''
+sieve = [True] * 101
+print(sieve)
+for i in range(2,100):
+	if sieve[i]:
+		print(i)
+		for j in range(i*i, 100, i):
+			sieve[j] = False
